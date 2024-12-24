9eea25b801265cd2d21e1f2676599d101f7ac08a
dohorcadrg <glorylegaspi@gmail.com>
2024-12-24 15:24:11 +1000

    Updated links

--- glorymodalbehavior.js
+++ glorymodalbehavior.js
@@ -7,6 +7,7 @@ if (exampleModal) {
     const modelimgsrc = button.getAttribute('src')
     const modeltitle = button.getAttribute('data-bs-modeltitle')
     const modelimgdescrip = button.getAttribute('data-bs-modeldescript')
+    const modelpicdate = button.getAttribute('data-bs-picdate')
     // If necessary, you could initiate an Ajax request here
     // and then do the updating in a callback.
 
@@ -14,10 +15,12 @@ if (exampleModal) {
     const modalTitle = exampleModal.querySelector('.modal-title')
     const modalBodyInput = exampleModal.querySelector('.modal-body img')
     const modalDescrip = exampleModal.querySelector('.modelimgdescrip')
+    const modalDateDisplay = exampleModal.querySelector('.picdate')
 
     modalTitle.textContent = modeltitle
     modalBodyInput.src = modelimgsrc
     modalDescrip.textContent = modelimgdescrip
+    modalDateDisplay.textContent = modelpicdate
 
   })
 }
